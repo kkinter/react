f22afdbc0a05f64afa95d10ef6e7a02a131b19d1
wook <ifol1129@gmail.com>
2022-11-10 02:49:35 +0900

    React Events

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 import Header from './components/header';
 import Footer from './components/footer';
 
+function createAlert(){
+    alert('hello. You clicked me');
+  }
+
+
 function App() {
   return (
     <div className="App">
-      <Header info="this is MY msg"
-      myNumber="6"/>
+      <Header info="this is MY msg"/>
       <p>main content</p>
-      <Footer trademark="page by Wook"/>
+      <Footer trademark="page by Wook"
+        myalert={createAlert}/>
     </div>
   );
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
 
 class Footer extends Component{
+
+  // createAlert(){
+  //   alert('hello. You clicked me');
+  // }
+
+  changed() {
+    console.log('chaged')
+  }
+
   render() {
-    return <h2>{this.props.trademark}</h2>
+    return (
+    <React.Fragment>
+      <h2 onClick={this.props.myalert}>
+        {this.props.trademark}
+      </h2>
+      <input onChange={this.changed} type="text"/>
+    </React.Fragment>
+    )
   }
 }
 
