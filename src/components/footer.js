2e25da757d2ac3ad2926df024a5b30b5bbb835a0
wook <ifol1129@gmail.com>
2022-11-10 13:05:02 +0900

    React conditional display

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,6 +10,7 @@ class Footer extends Component{
   state = {
     name: '',
     age: 35,
+    isLogin: true,
   }
 
   componentDidMount(){
@@ -29,15 +30,22 @@ class Footer extends Component{
 
   render() {
     return (
-    <React.Fragment>
-      <h2 onClick={this.props.myalert}>
+      <div>
+      { this.state.isLogin ? (
+      <React.Fragment>
+        <h2 onClick={this.props.myalert}>
         {this.props.trademark}
-      </h2>
-      <input value={this.state.name}
-          onChange={this.changed} type="text"/>
-      {/* 35면 yes */}
-      { this.state.age === 35 ? "yes": "no"}
+        </h2>
+        <input value={this.state.name}
+        onChange={this.changed} type="text"/>
     </React.Fragment>
+    ) : (
+      <React.Fragment>
+        <h2>You can't see this content</h2>
+        <h2>You must be login</h2>
+    </React.Fragment>
+    )}
+    </div>
     )
   }
 }
