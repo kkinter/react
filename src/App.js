5602189965e2f878e65eca8e2e3767c92e53c251
wook <ifol1129@gmail.com>
2022-11-10 12:55:41 +0900

    React ?

--- src/App.js
+++ src/App.js
@@ -8,16 +8,31 @@ function createAlert(){
     alert('hello. You clicked me');
   }
 
+function ShowMessage(props){
+  if(props.toShow){
+    return <h2>My msg</h2>
+  } else {
+    return <h2>Forbidden</h2>
+  }
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <Header info="this is MY msg"/>
-      <p>main content</p>
-      <Footer trademark="page by Wook"
-        myalert={createAlert}/>
-    </div>
-  );
+  const userlogin = true;
+  if (userlogin) {
+    return (
+      <div className="App">
+        <Header info="this is MY msg"/>
+        <p>main content</p>
+        <Footer trademark="page by Wook"
+          myalert={createAlert}/>
+          <ShowMessage toShow={true}/>
+      </div>
+    );
+  } else {
+    return <h2>Forbidden</h2>
+  }
+
 }
 
 export default App;
